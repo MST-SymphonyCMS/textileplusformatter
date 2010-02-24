385b429ae17e7a4f33df9f5a57bd83af8193cbc2
nick <nick@devo.randb.com.au>
2010-02-24 14:19:22 +0800

    Added change to textile class

--- lib/lib.textile.php
+++ lib/lib.textile.php
@@ -466,8 +466,9 @@ class Extension_TextPlusFormatter_Textile
     function fList($m)
     {
         $text = explode("\n", $m[0]);
-        foreach($text as $line) {
-            $nextline = next($text);
+		foreach($text as $nr => $line) {
+		  $nextline = isset($text[$nr+1]) ? $text[$nr+1] : false;
+
             if (preg_match("/^([#*]+)($this->a$this->c) (.*)$/s", $line, $m)) {
                 list(, $tl, $atts, $content) = $m;
                 $nl = '';
